0c45f528de55a6892cba375ac7440dd0cb3017a4
mmhalilovi1 <108989694+mmhalilovi1@users.noreply.github.com>
2025-05-31 11:30:22 +0200

    Uradjen task 9

--- BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
+++ BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e682acc98f3166d3739d9181788e9adb1ffad064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e71c3c7cc4ad568ad207df995494b0e596615c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
