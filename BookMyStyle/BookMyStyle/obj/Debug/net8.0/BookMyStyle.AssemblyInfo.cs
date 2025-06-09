4da5ed1866fdbb1c8a6370cc362acea15529cfd2
akanlic2 <akanlic2@etf.unsa.ba>
2025-06-09 21:38:34 +0200

    Dodan prikaz cijene

--- BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
+++ BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddb49865d3dd163b89af1ef03ccd01b3002ffdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0f1d9ef8844eb1d971a48ffc691d17b78fa967")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
