f10386f6736fe8ee70b6a9f4ec98f69a39f53162
Emina Hamamdzic <ehamamdzic1@etf.unsa.ba>
2025-06-10 21:00:23 +0200

    termin recenzija final

--- BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
+++ BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078a996f05fd2262e2392689492fdf6ecefa8f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e91b3685d2c4f7cbbf79276eefe81ed61204257")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
