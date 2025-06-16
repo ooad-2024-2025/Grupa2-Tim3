1df085619af7d40924526afd4dd392506e7f2072
mmhalilovi1 <108989694+mmhalilovi1@users.noreply.github.com>
2025-06-16 21:23:50 +0200

    Deployment

--- BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
+++ BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4809d1c205beb4f30b9e2be18df60e4f7813cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2da1a086510a4113545de4ba54c5ba97ed51e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
