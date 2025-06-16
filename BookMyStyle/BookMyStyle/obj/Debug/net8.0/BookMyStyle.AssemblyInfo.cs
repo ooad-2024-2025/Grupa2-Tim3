cb070baaf170ae19ea864fd9a4daa2c2585f8650
Ajnur Kušundžija <akusundzij1@etf.unsa.ba>
2025-06-17 01:01:51 +0200

    qr code scan css fixed and deployed

--- BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
+++ BookMyStyle/BookMyStyle/obj/Debug/net8.0/BookMyStyle.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df085619af7d40924526afd4dd392506e7f2072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fedc7c40d25a19ffb7c9f7c5500974d5946b981")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMyStyle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
